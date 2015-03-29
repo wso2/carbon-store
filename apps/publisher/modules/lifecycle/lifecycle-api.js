f18e7d8e89535c7124a0bff893fd7306762aed3c
sameeram <mgjaye@gmail.com>
2015-03-29 14:52:51 +0530

    Removed extra logs

--- apps/publisher/modules/lifecycle/lifecycle-api.js
+++ apps/publisher/modules/lifecycle/lifecycle-api.js
@@ -81,10 +81,8 @@ var error = '';
      */
     var resolveLifecycle = function(options, asset) {
         if (options.lifecycle) {
-            log.info('User has provided the lifecycle '+options.lifecycle);
             return options.lifecycle;
         }
-        log.info('Obtaining default lifecycle '+asset.lifecycle);
         return asset.lifecycle;
     };
     /**
