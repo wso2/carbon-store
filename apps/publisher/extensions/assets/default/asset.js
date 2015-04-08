553eff9b6994ae35bce741ed6f551d4d881b9331
Dhanuka De Silva <dhanukad@wso2.com>
2015-04-08 15:21:18 +0530

    Fixing styling issues

--- apps/publisher/extensions/assets/default/asset.js
+++ apps/publisher/extensions/assets/default/asset.js
@@ -201,17 +201,17 @@ asset.renderer = function (ctx) {
     var type = ctx.assetType;
     var buildListLeftNav = function(page, util) {
         var navList = util.navList();
-        navList.push('Add ' + type,'fa-plus', util.buildUrl('create'));
-        navList.push('Statistics', 'fa-area-chart', '/asts/' + type + '/statistics');
+        navList.push('Add ' + type,'btn-add-new', util.buildUrl('create'));
+        navList.push('Statistics', 'btn-stats', '/asts/' + type + '/statistics');
         //navList.push('Configuration', 'icon-dashboard', util.buildUrl('configuration'));
         return navList.list();
     };
     var buildDefaultLeftNav = function(page, util) {
         var id = page.assets.id;
         var navList = util.navList();
-        navList.push('Edit', 'fa-pencil', util.buildUrl('update') + '/' + id);
-        navList.push('Overview', 'fa-list-alt', util.buildUrl('details') + '/' + id);
-        navList.push('Life Cycle' , 'fa-recycle', util.buildUrl('lifecycle') + '/' + id);
+        navList.push('Edit', 'btn-edit', util.buildUrl('update') + '/' + id);
+        navList.push('Overview', 'btn-overview', util.buildUrl('details') + '/' + id);
+        navList.push('Life Cycle' , 'btn-lifecycle', util.buildUrl('lifecycle') + '/' + id);
         return navList.list();
     };
     var buildAddLeftNav = function (page, util) {
