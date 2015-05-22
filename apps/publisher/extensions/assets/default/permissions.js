3dcc617c5a08c6c17b16e02b7778dd202d86419e
sameeram <mgjaye@gmail.com>
2015-05-22 17:03:48 +0530

    Added correct way to check permissions

--- apps/publisher/extensions/assets/default/permissions.js
+++ apps/publisher/extensions/assets/default/permissions.js
@@ -1,16 +1,3 @@
 var permissions = {
-	'/store':{
-		"/pages":{
-			"/top-assets":"/read"
-		},
-		"/apis":{
-			"/assets":["/create","/update","/search","/remove"],
-			"/asset": ["/tags","/add-tags","/remove-tags"]
-		},
-		"/asts":{
-			"/details"
-			"/api":["/details","/create","/update"]
-			"/gadget"
-		}
-	}
-}
+	
+};
