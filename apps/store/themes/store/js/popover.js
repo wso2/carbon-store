1bc633d515250755b9c69991072fc6719152c30a
Jerad Rutnam <jeradrutnam@outlook.com>
2015-06-29 14:55:49 +0530

    Added custom popover drop-down menu function for both publisher & store

---
+++ apps/store/themes/store/js/popover.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+var popoverFunction = (function(){
+    var returnObj = {};
+    returnObj.init = function(){
+
+        var btn = '[data-click-event=popover]';
+
+        /**
+         * button click function
+         */
+        $(btn).click(function(e){
+            e.preventDefault();
+            $(this).next('.popover').toggle(0).toggleClass('in');
+        });
+        
+        /*
+         * Bind popover hide function to the document.
+         */
+        $(document).bind('click', function(e) {
+            if(($(e.target).closest(btn).length !== 1) && ($(e.target).closest('.popover').length === 0)){
+                $(btn).next('.popover').removeClass('in').hide();
+            }
+        });
+
+    };
+    return returnObj;
+})();
+
+$(function(){
+    popoverFunction.init();
+});
