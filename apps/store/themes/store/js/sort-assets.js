ea5c3b537a3f7780e010098945843fbf1bb21820
Rushmin Fernando <rushmin@wso2.com>
2015-08-24 20:34:02 +0530

    Cleaning the content inherited from master branch.

--- apps/store/themes/store/js/sort-assets.js
+++
@@ -1,9 +0,0 @@
-/*
-var updateSortUI = function() {
-	$('#ul-sort-assets').find('a[data-sort="' + store.asset.paging.sort + '"]').addClass('selected-type');
-	store.asset.count = 0;
-};
-
-$(function() {
-	updateSortUI();
-});*/
