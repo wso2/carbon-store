5cd54167fff83e9bbcac26ec8e6cd542c988c0fb
Chanaka Jayasena <chanaka@wso2.com>
2015-10-21 11:15:43 +0530

    Adding validation to the client side which are available with rxt

--- apps/publisher/themes/default/js/create_asset.js
+++ apps/publisher/themes/default/js/create_asset.js
@@ -16,58 +16,74 @@
  *  under the License.
  *
  */
-$(function(){
+$(function () {
+    validator.initValidationEvents('form-asset-create',function(){});
     $('#btn-create-asset').removeAttr('disabled');
-    var obtainFormMeta=function(formId){
-		return $(formId).data();
-	};
-    var populateTags = function(arr){
+    var obtainFormMeta = function (formId) {
+        return $(formId).data();
+    };
+    var populateTags = function (arr) {
         var entry;
-        for(var index = 0 ; index < arr.length; index++){
+        for (var index = 0; index < arr.length; index++) {
             entry = arr [index];
-            if(entry.name === '_tags'){
+            if (entry.name === '_tags') {
                 entry.value = tagsAPI.selectedTags();
             }
         }
     };
     $('#form-asset-create').ajaxForm({
-        beforeSubmit:function(arr){
+        beforeSubmit: function (arr) {
             var createButton = $('#btn-create-asset');
-            createButton.attr('disabled','disabled');
-            createButton.next().attr('disabled','disabled');
-            caramel.render('loading','Creating asset. Please wait..', function( info , content ){
-                createButton.parent().append($(content));
+            createButton.attr('disabled', 'disabled');
+            createButton.next().attr('disabled', 'disabled');
+            caramel.render('loading', 'Creating asset. Please wait..', function (info, content) {
+                var $content = $(content).removeClass('loading-animation-big').addClass('loading-animation');
+                createButton.parent().append($content);
             });
             populateTags(arr);
+            if (!validator.isValidForm('form-asset-create')) {
+                window.scrollTo(0, 0);
+                $('div.error').each(function () {
+                    var $container = $(this).closest('div.responsive-form-container');
+                    if (!$container.hasClass('in')) {
+                        $container.show('fast').addClass('in');
+                    }
+                });
+                $('#btn-create-asset').removeAttr('disabled');
+                setTimeout(
+                    function () {
+                        $('.loading-animation').remove();
+                    }, 1000);
+                return false;
+            }
         },
-        success:function(data){
-            var options=obtainFormMeta('#form-asset-create');
+        success: function (data) {
+            var options = obtainFormMeta('#form-asset-create');
             $('#btn-create-asset').removeAttr('disabled');
-            $.cookie("new-asset-"+data.type , data.id + ":" + data.type + ":" + data.name );
+            $.cookie("new-asset-" + data.type, data.id + ":" + data.type + ":" + data.name);
             window.location = options.redirectUrl;
         },
-        error:function(){
-            messages.alertError('Unable to add the '+PublisherUtils.resolveCurrentPageAssetType()+' instance.');
+        error: function () {
+            messages.alertError('Unable to add the ' + PublisherUtils.resolveCurrentPageAssetType() + ' instance.');
             $('#btn-create-asset').removeAttr('disabled');
             $('.fa-spinner').parent().remove();
-
         }
     });
-    var initDatePicker =  function(){
-        if($(this).attr('data-render-options') == "date-time"){
+    var initDatePicker = function () {
+        if ($(this).attr('data-render-options') == "date-time") {
             var dateField = this;
             $(this).DatePicker({
                 mode: 'single',
                 position: 'right',
-                onBeforeShow: function(el){
-                    if($(dateField).val().replace(/^\s+|\s+$/g,"")){
+                onBeforeShow: function (el) {
+                    if ($(dateField).val().replace(/^\s+|\s+$/g, "")) {
                         $(dateField).DatePickerSetDate($(dateField).val(), true);
                     }
 
                 },
-                onChange: function(date, el) {
-                    $(el).val((date.getMonth()+1)+'/'+date.getDate()+'/'+date.getFullYear());
-                    if($('#closeOnSelect input').attr('checked')) {
+                onChange: function (date, el) {
+                    $(el).val((date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear());
+                    if ($('#closeOnSelect input').attr('checked')) {
                         $(el).DatePickerHide();
                     }
                 }
@@ -78,46 +94,46 @@ $(function(){
     $('#form-asset-create input[type="text"]').each(initDatePicker);
 
 
-    var removeUnboundRow = function(link){
+    var removeUnboundRow = function (link) {
         var table = link.closest('table');
-        if($('tr',table).length == 2){
+        if ($('tr', table).length == 2) {
             table.hide();
         }
         link.closest('tr').remove();
     };
 
-    $('.js-add-unbounded-row').click(function(){
+    $('.js-add-unbounded-row').click(function () {
         var tableName = $(this).attr('data-name');
-        var table = $('#table_'+tableName);
+        var table = $('#table_' + tableName);
         table.find('thead').show();
-        var referenceRow = $('#table_reference_'+tableName);
+        var referenceRow = $('#table_reference_' + tableName);
         var newRow = referenceRow.clone().removeAttr('id');
         $('input[type="text"]', newRow).val('');
         table.show().append(newRow);
         table.find('thead').show();
-        $('input[type="text"]',newRow).each(initDatePicker);
+        $('input[type="text"]', newRow).each(initDatePicker);
 
     });
-    $('.js-unbounded-table').on('click','a',function(event){
+    $('.js-unbounded-table').on('click', 'a', function (event) {
         removeUnboundRow($(event.target));
     });
 
-    $('.js-unbounded-table').on('click','input[type="checkbox"]',function(event){
+    $('.js-unbounded-table').on('click', 'input[type="checkbox"]', function (event) {
         var checkbox = event.target;
         var hiddenField = $(checkbox).next();
-        if($(checkbox).is(":checked")){
+        if ($(checkbox).is(":checked")) {
             $(hiddenField).val('on');
-        }else{
+        } else {
             $(hiddenField).val('off');
         }
     });
 
-    $('.tmp_refernceTableForUnbounded').each(function(){
-        $(this).detach().attr('class','refernceTableForUnbounded').appendTo('body');
+    $('.tmp_refernceTableForUnbounded').each(function () {
+        $(this).detach().attr('class', 'refernceTableForUnbounded').appendTo('body');
     });
 
-    $('.tmp_refernceTableForOptionText').each(function(){
-        $(this).detach().attr('class','refernceTableForUnbounded').appendTo('body');
+    $('.tmp_refernceTableForOptionText').each(function () {
+        $(this).detach().attr('class', 'refernceTableForUnbounded').appendTo('body');
     });
 
 });
