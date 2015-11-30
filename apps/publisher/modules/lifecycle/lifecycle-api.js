d2668ddba628136b48553dff86e34473fd5f9afd
sameerak <dlskannangara@gmail.com>
2015-11-30 16:49:16 +0530

    STORE-1138 - using isLCPermitted for LC permission check instead of isLCActionsPermitted

--- apps/publisher/modules/lifecycle/lifecycle-api.js
+++ apps/publisher/modules/lifecycle/lifecycle-api.js
@@ -601,7 +601,6 @@ var error = '';
     };
     var isLCPermitted = function (asset, session) {
         var permissions = require('/modules/lifecycle/permissions.js').permissions;
-        log.info("asset = " + stringify(asset));
-        return permissions.isLCActionsPermitted(asset.type, session);
+        return permissions.isLCPermitted(asset.type, session);
     };
 }(api));
