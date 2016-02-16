acad1d949da956663c1075a96b104dbef02352e2
sameeram <mgjaye@gmail.com>
2016-02-16 17:25:08 +0530

    Initial commit for user review tests

---
+++ apps/store/modules/test/test-utils.js
@@ -0,0 +1,82 @@
+/**
+ * To add a asset and return the retrieved id of newly added asset
+ * @return uuid
+ */
+var getAssetID = function (asset_name) {
+    if(!asset_name){
+        var uuid = require('uuid');
+        asset_name = uuid.generate();
+    }
+    var url = server_url + '/assets?type=gadget';
+    var asset = {'overview_name': asset_name,
+        'overview_version': '1.2.3',
+        'overview_provider': 'admin',
+        'overview_description': 'initial description',
+        'overview_category': 'Google'};
+    var header = obtainAuthorizedHeaderForAPICall();
+    var response;
+    try {
+        response = post(url, asset, header, 'json');
+    } catch (e) {
+        log.error(e);
+    } finally {
+        assetId = response.data.id;
+        logoutAuthorizedUser(header);
+        expect(response.data).not.toBe(undefined);
+    
+    return response.data.id;
+};
+
+/**
+ *
+ * @return {{Cookie: string}}
+ */
+var obtainAuthorizedHeaderForAPICall = function (server_url,username,password) {
+    var authenticate = post(server_url + '/authenticate', {"password": password, "username": username }, {}, 'json');
+    var header = {
+        'Cookie': "JSESSIONID=" + authenticate.data.data.sessionId + ";",
+        'Accept': 'application/json'
+    };
+    return header;
+};
+
+/**
+ * The function to send logout request to publisher API
+ * @param header
+ */
+var logoutAuthorizedUser = function (header) {
+    post(server_url + '/logout', {}, header, 'json');
+};
+
+/**
+ * This function will send delete request for given asset id
+ * @param id The uuid of asset to be deleted
+ */
+var deleteAssetWithID = function (id) {
+    var url = server_url + '/assets/' + id + '?type=gadget';
+    var header = obtainAuthorizedHeaderForAPICall();
+    var response;
+    try {
+        response = del(url, {}, header);
+    } catch (e) {
+        log.error(e);
+    } finally {
+        logoutAuthorizedUser(header);
+        expect(response.xhr.status).toEqual(200);
+    }
+};
+
+var createReview = function(){
+};
+
+var deleteReview = function(reviewId){
+};
+
+var listReviews = function(){
+};
+
+var likeReview = function(){
+};
+
+var unlikeReview = function(){
+};
