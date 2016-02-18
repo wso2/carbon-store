15cc80e4e2a733d976e9eb1423810f0eeb59787f
sameeram <mgjaye@gmail.com>
2016-02-18 12:46:02 +0530

    Implemented create review test

--- apps/store/modules/test/test-utils.js
+++ apps/store/modules/test/test-utils.js
@@ -22,7 +22,7 @@ var util = {};
      * To add a asset and return the retrieved id of newly added asset
      * @return uuid
      */
-    util.getAssetID = function(asset_name) {
+    util.getAssetID = function(asset_name, header, server_url) {
         if (!asset_name) {
             var uuid = require('uuid');
             asset_name = uuid.generate();
@@ -35,7 +35,7 @@ var util = {};
             'overview_description': 'initial description',
             'overview_category': 'Google'
         };
-        var header = obtainAuthorizedHeaderForAPICall();
+        //var header = obtainAuthorizedHeaderForAPICall();
         var response;
         try {
             response = post(url, asset, header, 'json');
@@ -43,7 +43,7 @@ var util = {};
             log.error(e);
         } finally {
             assetId = response.data.id;
-            logoutAuthorizedUser(header);
+            //this.logoutAuthorizedUser(header, server_url);
             expect(response.data).not.toBe(undefined);
             return response.data.id;
         }
@@ -67,29 +67,129 @@ var util = {};
      * The function to send logout request to publisher API
      * @param header
      */
-    util.logoutAuthorizedUser = function(header) {
+    util.logoutAuthorizedUser = function(header, server_url) {
         post(server_url + '/logout', {}, header, 'json');
     };
     /**
      * This function will send delete request for given asset id
      * @param id The uuid of asset to be deleted
      */
-    util.deleteAssetWithID = function(id) {
+    util.deleteAssetWithID = function(id, header, server_url) {
         var url = server_url + '/assets/' + id + '?type=gadget';
-        var header = obtainAuthorizedHeaderForAPICall();
         var response;
         try {
             response = del(url, {}, header);
         } catch (e) {
             log.error(e);
         } finally {
-            logoutAuthorizedUser(header);
             expect(response.xhr.status).toEqual(200);
         }
     };
-    util.createReview = function() {};
-    util.deleteReview = function(reviewId) {};
-    util.listReviews = function() {};
-    util.likeReview = function() {};
-    util.unlikeReview = function() {};
+    util.createReview = function(review, header, server_url) {
+        var url = server_url + '/user-reviews';
+        var response = {};
+        header['Content-Type'] = 'application/json';
+        try {
+            response = post(url, JSON.stringify(review), header, 'application/json');
+        } catch (e) {
+            log.error(e);
+        } finally {
+            expect(response.xhr.status).toEqual(200);
+        }
+        return response.data;
+    };
+    util.deleteReview = function(reviewId, header, server_url) {
+        var url = server_url + '/user-reviews/' + reviewId;
+        try {
+            response = del(url, {}, 'application/json', header);
+        } catch (e) {
+            log.error(e);
+        } finally {
+            expect(response.xhr.status).toEqual(200);
+        }
+    };
+    util.listReviews = function(target, header, server_url, paging) {
+        var url = server_url + '/user-reviews';
+        var response = {};
+        var queryParams = {};
+        paging = paging || {};
+        if (paging.sortBy) {
+            queryParams.sortBy = paging.sortBy;
+        }
+        queryParams.target = target;
+        try {
+            response = get(url, queryParams, header);
+        } catch (e) {
+            log.error(e);
+        }
+        return response.data;
+    };
+    util.listPopularReviews = function(target, header, server_url) {
+        return this.listReviews(target, header, server_url, {
+            sortBy: 'POPULAR'
+        });
+    }
+    util.listNewestReviews = function(target, header, server_url) {
+        return this.listReviews(target, header, server_url, {
+            sortBy: 'NEWEST'
+        });
+    }
+    util.listOldestReviews = function(target, header, server_url) {
+        return this.listReviews(target, header, server_url, {
+            sortBy: 'OLDEST'
+        });
+    }
+    util.likeReview = function(target,header,server_url,review) {
+        action = this.generateActionMessage({
+            id:review.id,
+            target:target,
+            verb:'like'
+        });
+        return this.createReview(action,header,server_url);
+    };
+    util.unlikeReview = function(target,header,server_url,review) {
+        action = this.generateActionMessage({
+            id:review.id,
+            target:target,
+            verb:'unlike'
+        });
+        return this.createReview(action,header,server_url); 
+    };
+    /**
+     * Generates a template review object
+     */
+    util.generateReview = function(opts) {
+        var content = opts.content || 'This an empty review';
+        var rating = opts.rating || 2;
+        var target = opts.type + ':' + opts.id;
+        return {
+            "verb": "post",
+            "object": {
+                "objectType": "review",
+                "content": content,
+                "rating": rating,
+                "likes": {
+                    "totalItems": 0
+                },
+                "dislikes": {
+                    "totalItems": 0
+                }
+            },
+            "target": {
+                "id": target
+            }
+        };
+    };
+    util.generateActionMessage = function(opts) {
+        return {
+            "target": {
+                "id": opts.reviewId
+            },
+            "object": {},
+            "verb": opts.verb,
+            "context": {
+                "id": opts.target
+            }
+        };
+    };
 }(util));
--- apps/store/test/Reviews_spec.js
+++ apps/store/test/Reviews_spec.js
@@ -19,24 +19,254 @@
 var config = require('/test/conf.json');
 var publisherAPIURL = config.baseAPIUrls.publisher;
 var storeAPIURL = config.baseAPIUrls.store;
-var TestUtils = require('/moduldes/test/test-utils.js');
+var TestUtils = require('/modules/test/test-utils.js').util;
+var ASSET_TYPE = 'gadget';
+var REVIEW_CONTENT = ['Review 1', 'Review 2'];
 describe('User Review POST - Store API', function() {
-    it('Test add user review', function() {
-        var username = 'admin';
-        var password = 'admin';
-        //Authenticate
-        var publisherHeader = util.obtainAuthorizedHeaderForAPICall(publisherAPIURL, username, password);
-        var storeHeader = util.obtainAuthorizedHeaderForAPICall(storeAPIURL, username, password);
-
-        log.info('## STORE HEADER ##');
-        log.info(storeHeader);
-        log.info('## PUBLISHER HEADER ##');
-        log.info(publisherHeader);
-        //Create a new asset
-        expect(true).toBe(false);
+    it('Test adding a user review', function() {
+        var context = createContext(config);
+        var reviews;
+        var createdReviews;
+        var retrievedReviews;
+        try {
+            setup(context);
+            reviews = generateReviews(context, [reviewContent(1)]);
+            createdReviews = createReviews(context, reviews);
+            //Validate if the review API call was successfull
+            expect(createdReviews.length > 0).toBe(true);
+            expect(createdReviews[0].success).toBe(true);
+            //Retrieve the review and check if the review was created successfully
+            retrievedReviews = retrieveReviews(context);
+            //Validate the created review
+            expect(retrievedReviews.length > 0).toBe(true);
+            expect(retrievedReviews[0].object.content === reviewContent(1)).toBe(true);
+            //TODO: Remove the check for a rating of 2
+            expect(retrievedReviews[0].object.rating === 2).toBe(true);
+        } catch (e) {
+            log.error('Failed to test creation of a user review', e);
+            failTest();
+        } finally {
+            tearDown(context);
+        }
     });
-    it('Test delete user review', function() {});
-    it('Test liking a user review', function() {});
+    it('Test retrieving a list of user reviews', function() {
+        var context = createContext(config);
+        var reviews;
+        var createdReviews;
+        var retrievedReviews;
+        try {
+            setup(context);
+            reviews = generateReviews(context, [reviewContent(1), reviewContent(2)]);
+            createdReviews = createReviews(context, reviews);
+            retrievedReviews = retrieveReviews(context);
+            //Determine if the created count and retrieved count are equal
+            expect(retrievedReviews.length === createdReviews.length).toBe(true);
+            //Do a content comparison check
+            sortReviewsById(createdReviews);
+            sortReviewsById(retrievedReviews);
+            expect(matchReviewsByContent(createdReviews, retrievedReviews)).toBe(true);
+        } catch (e) {
+            log.error('Failed to test retrieval of user reviews', e);
+            failTest();
+        } finally {
+            tearDown(context);
+        }
+    });
+    it('Test deleting a user review', function() {
+        var context = createContext(config);
+        var reviews;
+        var createdReviews;
+        var retrievedReviews;
+        var retrievedReviewsAfterDelete;
+        var deletedReviews;
+        try {
+            setup(context);
+            reviews = generateReviews(context, [reviewContent(1), reviewContent(2)]);
+            createdReviews = createdReviews(context, reviews);
+            retrievedReviews = retrieveReviews(context);
+            sortReviewsById(createdReviews);
+            sortReviewsById(retrievedReviews);
+            //Delete the first review
+            deletedReviews = deleteReviews(context, reviews[0]);
+            retrievedReviewsAfterDelete = retrieveReviews(context);
+            //Check if the review count  as been reduced
+            var isReviewCountOneLess = (retrievedReviewsAfterDelete.length < retrievedReviews.length);
+            expect(isReviewCountOneLess).toBe(true);
+            //Check if the correct review has been deleted
+            var isReviewDeleted = matchReviewsById(deletedReviews, retrievedReviewsAfterDelete);
+            expect(isReviewDeleted).toBe(true);
+        } catch (e) {
+            log.error('Failed to test deleting a single user review',e);
+            failTest();
+        } finally {
+            tearDown(context);
+        }
+    });
+    it('Test liking a user review', function() {
+        var context = createContext(config);
+        var reviews;
+        var createdReviews;
+        var retrievedReviews;
+        var retrievedReviewsAfterLiking;
+        try {
+            setup(context);
+            reviews = generateReviews(context, [reviewContent(1)]);
+            createdReviews = createReviews(context, reviews);
+            retrievedReviews = retrieveReviews(context);
+            likeReview(retrievedReviews[0]);
+            retrievedReviewsAfterLiking = retrieveReviews(context);
+            var hasReviewBeenLiked = isLiked(retrievedReviewsAfterLiking[0]);
+            expect(hasReviewBeenLiked).toBe(true);
+        } catch (e) {
+            log.error(e);
+            failTest();
+        } finally {
+            tearDown(context);
+        }
+    });
     it('Test unliking a user review', function() {});
-    it('Test adding a user review without a rating', function() {});
+    it('Test retrieving newest reviews', function() {});
+    it('Test retrieving oldest reviews', function() {});
+    it('Test retrieving popular reviews', function() {});
 });
+/**
+ * Creates a context with configuration
+ */
+var createContext = function(conf) {
+    var context = {};
+    context.username = conf.authConfiguration.username;
+    context.password = conf.authConfiguration.password;
+    context.storeAPIURL = conf.baseAPIUrls.store;
+    context.publisherAPIURL = conf.baseAPIUrls.publisher;
+    context.type = ASSET_TYPE;
+    return context;
+};
+/**
+ * Creates an asset and establishes the sessions
+ * to the Store and Publisher
+ */
+var setup = function(context) {
+    var username = context.username;
+    var password = context.password;
+    var publisherAPIURL = context.publisherAPIURL;
+    var storeAPIURL = context.storeAPIURL;
+    context.publisherHeader = TestUtils.obtainAuthorizedHeaderForAPICall(publisherAPIURL, username, password);
+    context.storeHeader = TestUtils.obtainAuthorizedHeaderForAPICall(storeAPIURL, username, password);
+    context.assetId = TestUtils.getAssetID('review-gadget', context.publisherHeader, publisherAPIURL);
+    expect(assetId).toBeDefined();
+    return context;
+};
+/**
+ * Deletes the created asset and destroys 
+ * active sessions
+ */
+var tearDown = function(context) {
+    var storeHeader = context.storeHeader;
+    var publisherHeader = context.publisherHeader;
+    var publisherAPIURL = context.publisherAPIURL;
+    var storeAPIURL = context.storeAPIURL;
+    var assetId = context.assetId;
+    if (context.assetId) {
+        TestUtils.deleteAssetWithID(context.assetId, publisherHeader, publisherAPIURL);
+    }
+    if (publisherHeader) {
+        TestUtils.logoutAuthorizedUser(publisherHeader, publisherAPIURL);
+    }
+    if (storeHeader) {
+        TestUtils.logoutAuthorizedUser(storeHeader, storeAPIURL);
+    }
+};
+var createReviews = function(context, reviews) {
+    var review;
+    var results = [];
+    var result;
+    var isNumber;
+    for (var index = 0; index < reviews.length; index++) {
+        review = reviews[index];
+        result = TestUtils.createReview(review, context.storeHeader, context.storeAPIURL);
+        expect(result).toBeDefined();
+        result = parse(result);
+        expect(result.success).toBe(true);
+        isNumber = (typeof result.id === 'number') ? true : false;
+        expect(isNumber).toBe(true);
+        result.pushedContent = review.object.content;
+        results.push(result);
+    }
+    return results;
+};
+var deleteReviews = function(context, reviews) {
+    var results = [];
+    var result;
+    var id;
+    reviews.forEach(function(review) {
+        id = review.id;
+        if (!id) {
+            log.error('Unable to locate review to delete review');
+            failTest();
+        }
+        result = TestUtils.deleteReview(id, context.header, context.storeAPIURL);
+        results.push(result.id);
+    });
+    return results;
+};
+var generateReviews = function(context, templates) {
+    var reviews = [];
+    var review;
+    var template;
+    var assetId = context.assetId;
+    for (var index = 0; index < templates.length; index++) {
+        template = templates[index];
+        review = TestUtils.generateReview({
+            id: assetId,
+            type: ASSET_TYPE,
+            content: template
+        });
+        reviews.push(review);
+    }
+    return reviews;
+};
+var retrieveReviews = function(context) {
+    var storeAPIURL = context.storeAPIURL;
+    var storeHeader = context.storeHeader;
+    var target = context.type + ':' + context.assetId;
+    var reviews = TestUtils.listReviews(target, storeHeader, storeAPIURL);
+    expect(reviews).toBeDefined();
+    return parse(reviews);
+};
+var sortReviewsById = function(reviews) {
+    reviews.sort(function(a, b) {
+        return (a.id > b.id);
+    });
+};
+var matchReviewsByContent = function(setA, setB) {
+    var match = true;
+    setA.forEach(function(a, index) {
+        match = (a.content === setB[index].content);
+    });
+    return match;
+};
+var matchReviewsById = function(setA, setB) {
+    var match = true;
+    setA.forEach(function(a, index) {
+        match = (a.id === b.id);
+    });
+    return match;
+};
+/**
+ * Simulate a failure 
+ */
+var failTest = function() {
+    expect(true).toBe(false);
+};
+var reviewContent = function(index) {
+    index--;
+    if ((index <= REVIEW_CONTENT.length) && (index >= 0)) {
+        return REVIEW_CONTENT[index];
+    }
+    log.error('Failing test as test review content could not be found');
+    failTest();
+};
+var likeReview = function(review) {};
+var unlikeReview = function(review) {};
+var isLiked = function(review) {};
+var isUnliked = function(review) {};
