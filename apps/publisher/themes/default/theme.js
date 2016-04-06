9ce4661db02727d9b5c3edac40d5958879f26705
sameeram <mgjaye@gmail.com>
2016-04-06 21:26:37 +0530

    Changed the label style for the create/update/details page

--- apps/publisher/themes/default/theme.js
+++ apps/publisher/themes/default/theme.js
@@ -279,11 +279,11 @@ var engine = caramel.engine('handlebars', (function() {
                 var isHidden= (field.hidden)?field.hidden:false;
 
                 if (!isHidden && field.type != "option-text"){
-                    output = '<label class="custom-form-label col-lg-2 col-md-2 col-sm-12 col-xs-12">' + (field.name.label || field.name.name);
+                    output = '<label class="custom-form-label col-lg-2 col-md-2 col-sm-12 col-xs-12 text-right">' + (field.name.label || field.name.name);
                     if( (typeof(field.required) == "boolean" && field.required) || (typeof(field.required) == "string" && field.required == "true" )){
                         output += '<sup class="required-field">*</sup>';
                     }
-                    output += '</label>';
+                    output += ' : </label>';
                 }
                 return output;
             };
