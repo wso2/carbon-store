222415106a64c674836fccc6ef9c059ccc43e7b9
sameeram <mgjaye@gmail.com>
2016-04-07 21:38:35 +0530

    Refactored the message.js script and added message.js to the store

--- apps/publisher/themes/default/js/messages.js
+++ apps/publisher/themes/default/js/messages.js
@@ -1,16 +1,19 @@
 var messages = {};
 messages.alertSuccess = function(value){
     $.notify.addStyle('happygreen', {
-        html: "<div><span data-notify-html/></div>",
+        html: "<div><i class='icon fw fw-info'></i> <strong>Success! </strong><span data-notify-html/></div>",
         classes: {
             base: {
                 "white-space": "nowrap",
-                "background-color": "lightgreen",
-                "padding": "10px"
+                "background-color": "#5CB85C",
+                "padding": "10px",
+                "font-family":"Open Sans",
+                "color":"white",
+                "font-weight":300
             },
             supergreen: {
                 "color": "white",
-                "background-color": "DarkGreen"
+                "background-color": "#5CB85C"
             }
         }
     });
@@ -23,16 +26,19 @@ messages.alertSuccess = function(value){
 };
 messages.alertError = function(value){
     $.notify.addStyle('sadred', {
-        html: "<div><span data-notify-html/></div>",
+        html: "<div><i class='icon fw fw-error'></i> <strong>Error! </strong><span data-notify-html/></div>",
         classes: {
             base: {
                 "white-space": "nowrap",
-                "background-color": "IndianRed",
-                "padding": "10px"
+                "background-color": "#D9534F",
+                "padding": "10px",
+                "font-family":"Open Sans",
+                "color":"white",
+                "font-weight":300
             },
             superred: {
                 "color": "white",
-                "background-color":  "Maroon"
+                "background-color":  "#D9534F"
             }
         }
     });
