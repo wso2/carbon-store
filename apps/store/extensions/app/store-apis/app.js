dc9c11cdb96670e41c7402daff14a4bffa636fda
Thushara Kasun Ranawaka <thushara35@gmail.com>
2016-04-28 22:21:36 +0530

    fix for STORE-1218

--- apps/store/extensions/app/store-apis/app.js
+++ apps/store/extensions/app/store-apis/app.js
@@ -65,8 +65,6 @@ app.apiHandlers = function(ctx) {
     return {
         onApiLoad: function() {
             if ((ctx.isAnonContext) && (ctx.endpoint.secured)) {
-                //ctx.res.status='401';//sendRedirect(ctx.appContext+'/login');
-                print('{ error:"Authentication error" }'); //TODO: Fix this to return a proper status code
                 return false;
             }
             return true;
