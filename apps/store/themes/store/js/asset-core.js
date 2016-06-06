19e1f5fadccbff4f3527d7046996edba9c54aee3
Kasun Thennakoon <tmkasun@gmail.com>
2016-06-06 18:09:32 +0530

    Fix for STORE-1330: Update remove bookmark process and do code refactoring fixing code styling

--- apps/store/themes/store/js/asset-core.js
+++ apps/store/themes/store/js/asset-core.js
@@ -2,7 +2,7 @@ var asset = {};
 
 (function (asset) {
     asset.process = function (type, path, destination, elem) {
-        if(!store.user) {
+        if (!store.user) {
             $('#modal-login').modal('show');
             return;
         }
@@ -12,7 +12,7 @@ var asset = {};
         $.ajax({
             url: caramel.url('/apis/subscriptions'),
             data: {type: type, asset: path, destination: encodeURIComponent(location.href)},
-            method:'POST',
+            method: 'POST',
             success: function (data) {
                 messages.alertSuccess("Successfully subscribed to asset");
                 $(elem).find("i").removeClass().addClass('fa fa-star');
@@ -21,12 +21,12 @@ var asset = {};
                     var elem = $(this);
                     asset.unsubscribeBookmark(elem.data('type'), elem.data('aid'), location.href, elem);
                 });
-                $(elem).attr('id','btn-remove-subscribe');
+                $(elem).attr('id', 'btn-remove-subscribe');
             },
             error: function () {
                 messages.alertError("Failed to bookmark this asset!");
                 $(elem).find('i').removeClass();
-                $(elem).unbind('click').css("background-color","rgba(213, 35, 35, 0.78)");
+                $(elem).unbind('click').css("background-color", "rgba(213, 35, 35, 0.78)");
                 $(elem).find('#main-bookmark').html("Error!");
             }
         });
@@ -34,19 +34,31 @@ var asset = {};
     };
 
     asset.unsubscribeBookmark = function (type, path, destination, elem) {
-        if(!store.user) {
+        if (!store.user) {
             $('#modal-login').modal('show');
             return;
         }
-        $(elem).find('.sub-bookmark').addClass('fa fa-spinner fa-spin');
+        $(elem).find("i").removeClass().addClass('fa fa-spinner fa-spin');
+        $(elem).find('#main-bookmark').html(" Removing...");
+        $(elem).unbind('click');
         $.ajax({
             url: caramel.url('/apis/subscriptions') + '?type=' + type + '&asset=' + path,
-            method:'DELETE',
+            method: 'DELETE',
+            dataType: 'text json',
             success: function (data) {
-                location.href = destination;
+                messages.alertSuccess("Successfully un-subscribed to asset");
+                $(elem).find("i").removeClass().addClass('fw fw-bookmark');
+                $(elem).find('#main-bookmark').html("Bookmark");
+                $(elem).click(function () {
+                    var elem = $(this);
+                    asset.process(elem.data('type'), elem.data('aid'), location.href, elem);
+                }).attr('id', 'btn-add-gadget');
             },
-            error: function () {
-                $(elem).find('.sub-bookmark').removeClass('fa fa-spinner fa-spin');
+            error: function (data) {
+                messages.alertError("Failed to un-bookmark this asset!");
+                $(elem).find('i').removeClass();
+                $(elem).unbind('click').css("background-color", "rgba(213, 35, 35, 0.78)");
+                $(elem).find('#main-bookmark').html("Error!");
             }
         });
     };
