6fd8e47bd59f5142c489900ae819b593663e04d1
Kasun Thennakoon <tmkasun@gmail.com>
2016-06-06 18:07:31 +0530

    Fix for STORE-1330: Update asset bookmark ajax process

--- apps/store/themes/store/js/asset-core.js
+++ apps/store/themes/store/js/asset-core.js
@@ -6,17 +6,28 @@ var asset = {};
             $('#modal-login').modal('show');
             return;
         }
-        $(elem).find('.sub-bookmark').addClass('fa fa-spinner fa-spin');
+        $(elem).find("i").removeClass().addClass('fa fa-spinner fa-spin');
+        $(elem).find('#main-bookmark').html(" Adding...");
+        $(elem).unbind('click');
         $.ajax({
             url: caramel.url('/apis/subscriptions'),
             data: {type: type, asset: path, destination: encodeURIComponent(location.href)},
             method:'POST',
             success: function (data) {
                 messages.alertSuccess("Successfully subscribed to asset");
-                $('i',elem).removeClass('store-bookmark').addClass('store-bookmarked');
+                $(elem).find("i").removeClass().addClass('fa fa-star');
+                $(elem).find('#main-bookmark').html("Bookmarked");
+                $(elem).click(function () {
+                    var elem = $(this);
+                    asset.unsubscribeBookmark(elem.data('type'), elem.data('aid'), location.href, elem);
+                });
+                $(elem).attr('id','btn-remove-subscribe');
             },
             error: function () {
-                $(elem).find('.sub-bookmark').removeClass('fa fa-spinner fa-spin');
+                messages.alertError("Failed to bookmark this asset!");
+                $(elem).find('i').removeClass();
+                $(elem).unbind('click').css("background-color","rgba(213, 35, 35, 0.78)");
+                $(elem).find('#main-bookmark').html("Error!");
             }
         });
         //location.href = caramel.context + '/apis/subscriptions?type=' + type + '&asset=' + path + '&destination=' + encodeURIComponent(location.href);
