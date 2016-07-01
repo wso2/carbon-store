fad87ab75c5d4380c436489d09af9361d2f08c61
Kasun Thennakoon <tmkasun@gmail.com>
2016-07-01 19:15:00 +0530

    Fix for STORE-1355: Disable image viwer modal when , when no image available

--- apps/publisher/themes/default/js/view-asset.js
+++ apps/publisher/themes/default/js/view-asset.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
     var image = $('.image-display');
     image.click(function () {
+        var noImage = $(this).data().error;
+        if (noImage) {
+            $(this).off('click').css({cursor: 'default'});
+            return false;
+        }
         messages.modal_pop({content: '<img class="img-responsive" src="' + $(this).attr('src') + '" />'});
     });
-    image.error(
-        function () {
-            $(this).unbind("click").css("cursor", "default")
-        });
 });
