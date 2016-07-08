b87f2b61be1078edb8aac684019f9a30bfad72ca
Thushara Kasun Ranawaka <thushara35@gmail.com>
2016-07-08 10:56:09 +0530

    Fixing code formatting issues found using findbug

--- components/asset-client/src/main/java/org/wso2/store/client/util/ArtifactUploadClientConstants.java
+++ components/asset-client/src/main/java/org/wso2/store/client/util/ArtifactUploadClientConstants.java
@@ -20,19 +20,19 @@
 package org.wso2.store.client.util;
 
 public class ArtifactUploadClientConstants {
-	
-	public static final String DEFAULT_HOST_NAME = "localhost";
-	public static final String DEFAULT_PORT = "9443" ;
-	public static final String DEFAULT_USER = "admin";
-	public static final String DEFAULT_PWD = "admin";
-	public static final String DEFAULT_CONTEXT = "publisher";
-	
-	public static Integer SUCCESS_CODE = 200;
-	public static String PUBLISHER_AUTHORIZATION_URL = "/apis/authenticate";
-	public static String PUBLISHER_URL = "/apis/assets";
-	public static String THUMBNAIL_PREFIX = "_thumbnail";
-	public static String RXT_URL = "/apis/rxt/list";
-	public static String RXT_ATTRIBUTES_FOR_GIVEN_TYPE = "/apis/rxt";
-	public static String RESOURCE_DIR_NAME = "resources";
-    public static String RESOURCE_FILE_TYPE = "asset";
+
+    public static final String DEFAULT_HOST_NAME = "localhost";
+    public static final String DEFAULT_PORT = "9443";
+    public static final String DEFAULT_USER = "admin";
+    public static final String DEFAULT_PWD = "admin";
+    public static final String DEFAULT_CONTEXT = "publisher";
+
+    public static Integer SUCCESS_CODE = 200;
+    public static final String PUBLISHER_AUTHORIZATION_URL = "/apis/authenticate";
+    public static final String PUBLISHER_URL = "/apis/assets";
+    public static String THUMBNAIL_PREFIX = "_thumbnail";
+    public static final String RXT_URL = "/apis/rxt/list";
+    public static final String RXT_ATTRIBUTES_FOR_GIVEN_TYPE = "/apis/rxt";
+    public static final String RESOURCE_DIR_NAME = "resources";
+    public static final String RESOURCE_FILE_TYPE = "asset";
 }
