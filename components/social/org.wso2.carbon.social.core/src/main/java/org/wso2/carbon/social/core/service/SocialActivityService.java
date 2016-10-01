a6b63663baa40847faa3eb7147aef5c0648d2c59
Kasun Thennakoon <tmkasun@gmail.com>
2016-10-01 15:24:39 +0530

    Fix for 643#pullrequestreview-2079955 update the  docs

--- components/social/org.wso2.carbon.social.core/src/main/java/org/wso2/carbon/social/core/service/SocialActivityService.java
+++ components/social/org.wso2.carbon.social.core/src/main/java/org/wso2/carbon/social/core/service/SocialActivityService.java
@@ -70,14 +70,16 @@ public abstract class SocialActivityService {
 		return serializedActivities;
 	}
 
-	/**
-	 * Update an activity given new activity
-	 *
-	 * @param activity
-	 * @throws SocialActivityException
-	 * @throws JsonSyntaxException
-	 */
-	public String update(String activity) throws SocialActivityException {
+    /**
+     * Update an activity given new activity,
+     * Retrieve the current activity by activitiId and Replace the existing activity information with the
+     * given {activity} object
+     * @param activity Stringify activity JSON, An activity can be either
+     *                       comment or like/dislike unlike/un-dislike
+     * @throws SocialActivityException
+     * @throws JsonSyntaxException
+     */
+    public String update(String activity) throws SocialActivityException {
 		return getActivityPublisher().update(activity);
 	}
 
