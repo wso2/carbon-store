b7704e6f455bdd14fd230e62ddeb2d2ccbf6364a
Malintha <Malintha@users.noreply.github.com>
2016-10-12 01:50:53 +0530

    Using !tagEntry instead of strict equality

--- apps/publisher/themes/default/js/create_asset.js
+++ apps/publisher/themes/default/js/create_asset.js
@@ -36,7 +36,7 @@ $(function () {
                 modifiedArr.push(entry);
             } else {
                 tagEntry = entry;
-                if (tagEntry === undefined) {
+                if (!tagEntry) {
                     return;
                 }
                 tagEntry.value = tagsAPI.selectedTags();
